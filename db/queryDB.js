b25f6c7984487610b5db2d2a7eacca70d686abdd
Joe McCabe <me@joemccabe.co.uk>
2016-09-20 21:36:28 +0100

    updated db config

--- db/queryDB.js
+++ db/queryDB.js
@@ -8,44 +8,48 @@ var pg = require('pg');
 var Promise = require('promise');
 
 var customConfig = {
-    user: 'ayoung', //env var: PGUSER
-    database: config.database.db,
-    password: 'Popcorn10',
-    port: config.database.port,
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+	user: 'docker',
+	database: 'docker',
+	password: process.env.PGPASSWORD,
+	host: process.env.DB_PORT_5432_TCP_ADDR,
+	port: process.env.DB_PORT_5432_TCP_PORT,
+	max: 10,
+	idleTimeoutMillis: 30000,
 };
 
 exports.queryDB = Promise.nodeify(connectionToDB);
 
 const pool = new pg.Pool(customConfig);
 
-function connectionToDB(sql){
+function connectionToDB(sql) {
 
-    return new Promise(function(resolve, reject){
-        var results = [];
-        try {
-            pool.connect(function (err, client, done) {
-                query = client.query(sql);
-                // Stream results back one row at a time
-                query.on('row', function (row) {
-                    if(row != "" | row != " ") {
-                        results.push(row);
-                    }
-                });
-                query.on('end', function () {
-                    done();
-                    toReturn = {};
-                    toReturn.error = null;
-                    toReturn.results = results;
-                    resolve(toReturn);
-                });
-            })
-        } catch (err) {
-            console.log("SQL ERROR" + err);
-            done();
-            reject({'data':null,'error':err})
-        }
-    });
+	return new Promise(function(resolve, reject) {
+		var results = [];
+		try {
+			pool.connect(function(err, client, done) {
+				query = client.query(sql);
+				// Stream results back one row at a time
+				query.on('row', function(row) {
+					if (row != "" | row != " ") {
+						results.push(row);
+					}
+				});
+				query.on('end', function() {
+					done();
+					toReturn = {};
+					toReturn.error = null;
+					toReturn.results = results;
+					resolve(toReturn);
+				});
+			})
+		} catch (err) {
+			console.log("SQL ERROR" + err);
+			done();
+			reject({
+				'data': null,
+				'error': err
+			})
+		}
+	});
 
 }
